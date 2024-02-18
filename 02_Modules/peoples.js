8b312c2541e22b7b21e2c322008a1791b540d2bf
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-18 11:41:55 -0500

    add: variables (peoples)

---
+++ 02_Modules/peoples.js
@@ -0,0 +1,12 @@
+let person1 = 'Daniel';
+let person2 = 'John';
+let person3 = 'David';
+
+// We can exports these variables as an object
+
+module.exports = {
+	person1,
+	person2,
+	person3,
+};
+// Now we can access these variables from another file
