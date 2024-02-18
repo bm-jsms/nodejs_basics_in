9c0d2d999e1a14127cf3394913ee85e40f96d76a
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-18 11:55:47 -0500

    add: use variables

--- 03_ES6_Imports/index.js
+++ 03_ES6_Imports/index.js
@@ -1,3 +1,6 @@
 import greet from './greet.js';
+import { p1, p2, p3 } from './peoples.js';
 
-greet('Daniel'); // Hello Daniel
+greet(p1); // Hello Daniel
+greet(p2); // Hello John
+greet(p3); // Hello David
