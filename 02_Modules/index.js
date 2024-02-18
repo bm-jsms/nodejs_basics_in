e42145f2efa59f69c2dc8f69a7db7a92f57daf80
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-18 11:43:27 -0500

    add: import variables from peoples.js

--- 02_Modules/index.js
+++ 02_Modules/index.js
@@ -1,4 +1,6 @@
 const greet = require('./greet'); // This is the only thing that is returned from the module
+const { person1, person2, person3 } = require('./peoples'); // We can access these variables from another file
 
-greet('John');
-greet('Jane');
+greet(person1); // Hello Daniel
+greet(person2); // Hello John
+greet(person3); // Hello David
