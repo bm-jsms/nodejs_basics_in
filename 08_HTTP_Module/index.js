6a289ef56808957b1cc3414e106aa9b66af926c3
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-19 00:14:22 -0500

    http: listen server

--- 08_HTTP_Module/index.js
+++ 08_HTTP_Module/index.js
@@ -4,3 +4,8 @@ import http from 'http';
 const server = http.createServer((req, res) => {
 	res.end('Hello World');
 });
+
+// Listen on port 8000
+server.listen(8000, () => {
+	console.log('Server is listening on port 8000');
+});
