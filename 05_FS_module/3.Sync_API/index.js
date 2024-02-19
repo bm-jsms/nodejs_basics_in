cf7248ca1fbccf2c2c5e32f1fbe3da96f37b9947
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-18 23:51:22 -0500

    fs Sync: remove dir

--- 05_FS_module/3.Sync_API/index.js
+++ 05_FS_module/3.Sync_API/index.js
@@ -2,3 +2,6 @@ import * as fs from 'fs';
 
 // Create a Directory
 fs.mkdirSync('./05_FS_module/3.Sync_API/newFolder', { recursive: true });
+
+// Remove Directory
+fs.rmdirSync('./05_FS_module/3.Sync_API/newFolder', { recursive: true });
