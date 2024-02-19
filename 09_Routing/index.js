a70251d7e5e18c25f276c1fc8bfb91a3f369be4a
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-19 00:32:09 -0500

    add: http - routes

--- 09_Routing/index.js
+++ 09_Routing/index.js
@@ -7,6 +7,10 @@ const server = http.createServer((req, res) => {
 	// Routing
 	if (req.url === '/') {
 		res.end('<h1>Home Page</h1>');
+	} else if (req.url === '/about') {
+		res.end('<h1>About Page</h1>');
+	} else {
+		res.end('<h1>404 Not Found</h1>');
 	}
 
 	res.writeHead(200, 'OK', {
