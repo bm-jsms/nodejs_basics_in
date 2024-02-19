eeee46fc8530f0fa98fe5ca7e1c7b0c96b2bf0af
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-19 00:31:26 -0500

    add: http - home content

--- 09_Routing/index.js
+++ 09_Routing/index.js
@@ -6,11 +6,7 @@ const port = 8000;
 const server = http.createServer((req, res) => {
 	// Routing
 	if (req.url === '/') {
-		res.write('<h1>Hello Nodejs</h1>');
-	} else if (req.url === '/about') {
-		res.write('<h1>About Nodejs</h1>');
-	} else {
-		res.write('<h1>404 Not Found</h1>');
+		res.end('<h1>Home Page</h1>');
 	}
 
 	res.writeHead(200, 'OK', {
