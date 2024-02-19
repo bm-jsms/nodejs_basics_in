bddc2f202ea8c81ee193a0987773020d5852f69f
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-19 00:19:25 -0500

    http: res - writeHead

--- 08_HTTP_Module/index.js
+++ 08_HTTP_Module/index.js
@@ -2,11 +2,15 @@ import http from 'http';
 
 // Create a server
 const server = http.createServer((req, res) => {
-	res.statusCode = 404;
+	/* res.statusCode = 404;
 	res.setHeader('Content-Type', 'text/html');
-	res.statusMessage = 'Not Found';
+	res.statusMessage = 'Not Found'; */
 
 	res.write('<h1>Hello Nodejs</h1>');
+
+	res.writeHead(200, 'OK', {
+		'Content-Type': 'text/html',
+	});
 });
 
 // Listen on port 8000
