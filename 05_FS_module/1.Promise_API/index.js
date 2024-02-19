a1263068c46236f67dca6b92bf2d8a1ef854f3d5
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-18 23:20:02 -0500

    fs: read content

--- 05_FS_module/1.Promise_API/index.js
+++ 05_FS_module/1.Promise_API/index.js
@@ -1,9 +1,17 @@
 import * as fs from 'fs/promises';
 
 // create directory / folder
-try {
+/* try {
 	await fs.mkdir('./05_FS_module/1.Promise_API/newFolder');
 	console.log('Folder created');
+} catch (err) {
+	console.error('Error:', err);
+} */
+
+// Read the content
+try {
+	const files = await fs.readdir('./05_FS_module/1.Promise_API');
+	console.log('Files:', files);
 } catch (err) {
 	console.error('Error:', err);
 }
